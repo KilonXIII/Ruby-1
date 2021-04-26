b3a6754dd4bcf94d101b1c0b13b987bee986f067
KilonXIII <linkzero1515@gmail.com>
2021-04-26 21:41:05 +0900

    Test
    저장 테스트

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -4,21 +4,21 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
-    // Start는 처음 시작되는 부분 - Git테스트중
-    // 가사가 다시 수정
+    
     void Start()
     {
-        
     }
-
-    // 화면 갱신될때마다 호출됨 - git테스트중
+    
     void Update()
+
+
     {
         float horizontal = Input.GetAxis("Horizontal");
-        Debug.Log(horizontal + "그리고 다른 부분을 추가 수정했다 - git test중");
+        float vertical = Input.GetAxis("Vertical");
 
         Vector2 position = transform.position;
-        position.x = position.x + 0.1f * horizontal;
+        position.x = position.x + 5.0f * horizontal * Time.deltaTime;
+        position.y = position.y + 5.0f * vertical * Time.deltaTime;
         transform.position = position;
     }
 }
